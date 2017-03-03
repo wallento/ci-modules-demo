dd7356767b11d461db34e69061232aefeb237f7b
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-03-03 09:35:40 +0100

    Switch to new tool build environment

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,5 @@
 JOBS=-j8
 
 mkdir -p tools
-docker run -ti -v ${PWD}/tools:/tools librecores/toolsbuild-cocotb:latest
-docker run -ti -v ${PWD}/tools:/tools -e JOBS=${JOBS} librecores/toolsbuild-icarus-verilog:latest
+docker run -ti -v ${PWD}/tools:/tools lccitools/cocotb:latest
+docker run -ti -v ${PWD}/tools:/tools -e JOBS=${JOBS} lccitools/icarus-verilog:latest
